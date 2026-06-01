6794a433e4fd7162a310fa0cd89e1af058dcacd6
B1naryBr0ther <BinaryBrother@gmail.com>
2026-06-01 18:40:12 -0500

    Var Renames

--- PingColors/Program.cs
+++ PingColors/Program.cs
@@ -6,21 +6,21 @@ namespace PingColors
     {
         static void Main(string[] args)
         {
-            int warningResponseTime = 80; // Default warning threshold in milliseconds
-            int criticalResponseTime = 200; // Default critical threshold in milliseconds
-            int timeout = 5000; // Default timeout for ping in milliseconds
-            bool speedMode = false; // Default speed mode is off
-            IPAddress host = null;
-            
+            int iWarningResponseTime = 80;       // Default warning threshold in milliseconds
+            int iCriticalResponseTime = 200;     // Default critical threshold in milliseconds
+            int iTimeout = 5000;                 // Default timeout for ping in milliseconds
+            bool bSpeedMode = false;             // Default speed mode is off
+            IPAddress oHost = IPAddress.None;    // Default host is none, will be set by CLI
+
             CLI commandLineInterface = new();
             Custom customMethods = new();
 
             Console.CancelKeyPress += delegate { Console.ResetColor(); Console.WriteLine("Exiting..."); };
             Console.Title = "PingColors";
 
-            commandLineInterface.HandleArgs(ref warningResponseTime, ref criticalResponseTime, ref timeout, ref host, ref speedMode, args);
-            customMethods.ErrorChecking(warningResponseTime, criticalResponseTime, timeout, host);
-            customMethods.Ping(host, timeout, warningResponseTime, criticalResponseTime, speedMode);
+            commandLineInterface.HandleArgs(ref iWarningResponseTime, ref iCriticalResponseTime, ref iTimeout, ref oHost, ref bSpeedMode, args);
+            customMethods.ErrorChecking(iWarningResponseTime, iCriticalResponseTime, iTimeout, oHost);
+            customMethods.Ping(oHost, iTimeout, iWarningResponseTime, iCriticalResponseTime, bSpeedMode);
         }
     }
 }
